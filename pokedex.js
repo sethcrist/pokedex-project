c0e4f91a7ca7c1da747aedd170fa9f9e46553872
Seth Crist <sethcrist50@gmail.com>
2024-01-18 17:18:49 -0600

    websit is pulling information from the api

--- pokedex.js
+++ pokedex.js
@@ -1,3 +1,5 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+
 const MAX_POKEMON = 151; //can change this to a higher number
 const listWrapper = document.querySelector(".list-wrapper");
 const searchInput = document.querySelector("#search-input");
@@ -10,6 +12,56 @@ let allPokemons = [];
 fetch(`https://pokeapi.co/api/v2/pokemon?limit=${MAX_POKEMON}`)
     .then((response) => response.json())
     .then((data) => {
-        allPokemons = data.result;
-        console.log(data);
+        allPokemons = data.results;
+        displayPokemons(allPokemons)
     });
+
+async function fetchPokemonDataBeforeRedirect(id) {
+    try {
+        const [pokemon, pokemonSpecies] = await Promise
+            .all([fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+            .then((res) =>
+                res.json()
+            ),
+                fetch(`https://pokeapi.co/api/v2/pokemon-species/${id}`)
+                .then((res) =>
+                res.json()
+                ),
+            ]);
+        return true
+    } catch (error) {
+        console.error("failed to fetch Pokemon data before redirect");
+    }
+}
+
+function displayPokemons(pokemon) {
+    listWrapper.innerHTML = "";
+
+    pokemon.forEach((pokemon) => {
+        const pokemonID = pokemon.url.split("/")[6];
+        const listItem = document.createElement("div");
+        listItem.className = "list-item";
+        listItem.innerHTML = `
+            <div class="number-wrap">
+                <p class="caption-fonts">#${pokemonID}</p>
+            </div>
+            <div class="img-wrap">
+                <img src="https://raw.githubusercontent.com/pokeapi/sprites/master/sprites/pokemon/other/dream-world/${pokemonID}.svg"
+                alt="${pokemon.name}">
+            </div>
+            <div class="name-wrap">
+            <p class="body3-fonts">#${pokemon.name}</p>
+        </div>
+        `;
+
+        listItem.addEventListener("click", async () => {
+            const success = await fetchPokemonDataBeforeRedirect(pokemonID);
+            if (success) {
+                window.location.href = `./detail.html?id=${pokemonID}`;
+            }
+        });
+
+        listWrapper.appendChild(listItem);
+    });
+}
+});
