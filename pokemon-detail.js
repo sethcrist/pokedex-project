cbac43f6f6732144ee57d6e20a265e877a40dcdf
Seth Crist <sethcrist50@gmail.com>
2024-01-23 12:23:58 -0600

    updated the status bars to reflect the type of pokemon

--- pokemon-detail.js
+++ pokemon-detail.js
@@ -88,6 +88,47 @@ const typeColors = {
 
 function setElementStyles(elements, cssProperty, value) {
     elements.forEach((element) => {
+        element.style[cssProperty] = value;
+    });
+}
 
-    })
+function rgbaFromHex(hexColor) {
+    return [
+        parseInt(hexColor.slice(1, 3), 16),
+        parseInt(hexColor.slice(3, 5), 16),
+        parseInt(hexColor.slice(5, 7), 16),
+    ].join(", ");
+}
+
+function setTypeBackgroundColor(pokemon) {
+    const mainType = pokemon.types[0].type.name;
+    const color = typeColors[mainType];
+
+    if (!color) {
+        console.warn(`Color not defined for type: ${mainType}`);
+        return;
+    }
+    const detailMainElement = document.querySelector(".detail-main");
+    setElementStyles([detailMainElement], "backgroundColor", color);
+    setElementStyles([detailMainElement], "borderColor", color);
+
+    setElementStyles(document.querySelectorAll(".power-wrapper > p"), "backgroundColor", color);
+    setElementStyles(document.querySelectorAll(".stats-wrap p.stats"), "color", color);
+    setElementStyles(document.querySelectorAll(".stats-wrap .progress-bar"), "color", color);
+
+    const rgbaColor = rgbaFromHex(color);
+    const styleTag = document.createElement("style");
+    styleTag.innerHTML = `
+        .stats-wrap .progress-bar::-webkit-progress-bar {
+            background-color: rgba(${rgbaColor}, 0.5);
+            }
+        .stats-wrap .progress-bar::-webkit-progress-value {
+            background-color: ${color};
+            }    
+    `;
+    document.head.appendChild(styleTag);
+}
+
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
 }
